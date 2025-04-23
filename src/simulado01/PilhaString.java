66863228d93e7c278f48618feab0a76e59031e0a
Giovane Barcelos <giovanebarcelos@gmail.com>
2025-04-23 19:44:54 -0300

    feat: pilha string

---
+++ src/simulado01/PilhaString.java
@@ -0,0 +1,33 @@
+package simulado01;
+
+/*
+  1 ponto
+  Implemente uma classe PilhaString com:
+  - Array de Strings (tamanho fixo 10)
+  - Métodos: empilhar(String), desempilhar(), estaVazia(), topo()
+*/
+
+public class PilhaString {
+    private String[] dados = new String[10];
+    private int topo = -1;
+
+    public void empilhar(String s) {
+        dados[++topo] = s;
+    }
+
+    public String desempilhar() {
+        return dados[topo--];
+    }
+
+    public boolean estaVazia() {
+        return topo == -1;
+    }
+
+    public String topo() {
+        if (!this.estaVazia()){
+            return this.dados[topo] ;
+        } else {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+}
